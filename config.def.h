ace855e1124b6da03d432056e576e3cc31b929d7
keni7385 <andrea.corsini@outlook.com>
2019-10-23 11:19:39 +0300

    Changed font

--- config.def.h
+++ config.def.h
@@ -18,4 +18,4 @@ static const char * message = "Suckless: Software that sucks less.";
 static const char * text_color = "#ffffff";
 
 /* text size (must be a valid size) */
-static const char * font_name = "6x10";
+static const char * font_name = "6x13";
