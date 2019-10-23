0a3d3c6322e00195a1b4b4882934f3fa10ef79ba
keni7385 <andrea.corsini@outlook.com>
2019-10-23 10:59:39 +0300

    Changed user and group

--- config.def.h
+++ config.def.h
@@ -1,6 +1,6 @@
 /* user and group to drop privileges to */
-static const char *user  = "nobody";
-static const char *group = "nogroup";
+static const char *user  = "andrea";
+static const char *group = "andrea";
 
 static const char *colorname[NUMCOLS] = {
 	[INIT] =   "black",     /* after initialization */
